09c03f4fe1cca71e6f0a37cec0d0da33ec87763d
Matthew Ristine <matthewristine@gmail.com>
2018-02-10 11:54:28 -0800

    Creates playground project to explore thirdparty options

---
+++ Playground/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibGit2Sharp;
+
+namespace Playground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                using (var repo = new Repository("C:\\source\\git-basic"))
+                {
+                    foreach (TreeEntryChanges c in repo.Diff.Compare<TreeChanges>())
+                    {
+                        DiffViewer.Text += $"{c}\n";
+                    }
+                }
+            });
+        }
+    }
+}
