7a6aa8a997ae04f9ef5944e55d5869a223fbb55b
Matthew Ristine <matthewristine@gmail.com>
2018-02-25 16:24:27 -0800

    Adds commands to all buttons

--- GitBasic/ViewModels/CommandButtonVM.cs
+++ GitBasic/ViewModels/CommandButtonVM.cs
@@ -7,9 +7,17 @@ namespace GitBasic
     public class CommandButtonVM
     {
         public Prop<HotKeyHelper> HotKeyHelper { get; set; } = new Prop<HotKeyHelper>();
-        public Action Fetch { get; set; }
-        public Action Commit { get; set; }
+
         public Action Status { get; set; }
+        public Action Fetch { get; set; }
+        public Action Checkout { get; set; }
+        public Action Commit { get; set; }
+        public Action AddAll { get; set; }
+        public Action CommitAll { get; set; }
+        public Action Pull { get; set; }
+        public Action Push { get; set; }
+        public Action Branch { get; set; }
+        public Action Merge { get; set; }
 
         public CommandButtonVM(MainVM mainVM)
         {
@@ -20,30 +28,65 @@ namespace GitBasic
 
         private void CreateCommands()
         {
-            Fetch = new Action(() => _mainVM.ConsoleControlVM.ExecuteCommand(GIT_FETCH));
-            Commit = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_COMMIT_ALL, GIT_COMMIT_ALL.Length - 1));
             Status = new Action(() => _mainVM.ConsoleControlVM.ExecuteCommand(GIT_STATUS));
+            Fetch = new Action(() => _mainVM.ConsoleControlVM.ExecuteCommand(GIT_FETCH));
+            Checkout = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_CHECKOUT, GIT_CHECKOUT.Length));
+            Commit = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_COMMIT, GIT_COMMIT.Length - 1));
+            AddAll = new Action(() => _mainVM.ConsoleControlVM.ExecuteCommand(GIT_ADD_ALL));
+            CommitAll = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_COMMIT_ALL, GIT_COMMIT_ALL.Length - 1));
+            Pull = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_PULL, GIT_PULL.Length));
+            Push = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_PUSH, GIT_PUSH.Length));
+            Branch = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_BRANCH, GIT_BRANCH.Length));
+            Merge = new Action(() => _mainVM.ConsoleControlVM.SetInputCommand(GIT_MERGE, GIT_MERGE.Length));
         }
 
         private void RegisterHotKeys()
         {
             if (HotKeyHelper.Value != null)
             {
-                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D1, ModifierKeys.Control, Fetch));
-                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad1, ModifierKeys.Control, Fetch));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D1, ModifierKeys.Control, Status));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad1, ModifierKeys.Control, Status));
 
-                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D2, ModifierKeys.Control, Commit));
-                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad2, ModifierKeys.Control, Commit));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D2, ModifierKeys.Control, Fetch));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad2, ModifierKeys.Control, Fetch));
 
-                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D3, ModifierKeys.Control, Status));
-                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad3, ModifierKeys.Control, Status));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D3, ModifierKeys.Control, Checkout));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad3, ModifierKeys.Control, Checkout));
 
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D4, ModifierKeys.Control, Commit));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad4, ModifierKeys.Control, Commit));
+
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D5, ModifierKeys.Control, AddAll));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad5, ModifierKeys.Control, AddAll));
+
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D6, ModifierKeys.Control, CommitAll));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad6, ModifierKeys.Control, CommitAll));
+
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D7, ModifierKeys.Control, Pull));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad7, ModifierKeys.Control, Pull));
+
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D8, ModifierKeys.Control, Push));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad8, ModifierKeys.Control, Push));
+
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D9, ModifierKeys.Control, Branch));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad9, ModifierKeys.Control, Branch));
+
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.D0, ModifierKeys.Control, Merge));
+                HotKeyHelper.Value.RegisterHotKey(new HotKey(Key.NumPad0, ModifierKeys.Control, Merge));
             }
-        }                       
-        
-        private const string GIT_FETCH = "git fetch";
-        private const string GIT_COMMIT_ALL = "git commit -a -m \"\"";
+        }
+
         private const string GIT_STATUS = "git status";
+        private const string GIT_FETCH = "git fetch";
+        private const string GIT_CHECKOUT = "git checkout";
+        private const string GIT_COMMIT = "git commit -m \"\"";
+        private const string GIT_ADD_ALL = "git add -A";
+        private const string GIT_COMMIT_ALL = "git commit -a -m \"\"";
+        private const string GIT_PULL = "git pull";
+        private const string GIT_PUSH = "git push";
+        private const string GIT_BRANCH = "git branch";
+        private const string GIT_MERGE = "git merge";
+
         private MainVM _mainVM;
     }
 }
