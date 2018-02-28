289d5cf60813a13be1b5c808284f4d3ad4f5b7db
gdb13 <gdb.thirteen@gmail.com>
2018-02-28 00:42:45 -0500

    Working on ItemProvider to build tree structure

--- Playground/Lib/ItemProvider.cs
+++ Playground/Lib/ItemProvider.cs
@@ -10,47 +10,97 @@ namespace Playground.Lib
         public List<Item> GetItems(string path)
         {
             var items = new List<Item>();
-
-            var repo = new Repository(path);
-
-            //var dirInfo = new DirectoryInfo(path);
-
-            foreach (var file in repo.RetrieveStatus())
+;           List<string> repo_items = new List<string>;
+            
+            if (Repository.IsValid(path))
             {
-                var item = new FileItem
+
+                var repo = new Repository(path);
+
+                var dir_info = new DirectoryInfo(path);
+
+                BuildTreeFromRepo file_tree = new BuildTreeFromRepo();
+
+                foreach (var repo_item in repo.RetrieveStatus())
                 {
-                    Name = file.FilePath,
-                    Path = file.FilePath
-                };
+                    repo_items.Add(repo_item.FilePath);
+                }
+                items = file_tree.RecurseRepoItems(repo_items, path);
 
-                items.Add(item);
-            }
 
-            /*
-            foreach (var directory in dirInfo.GetDirectories())
-            {
-                var item = new DirectoryItem
+                /*
+                foreach (var directory in dirInfo.GetDirectories())
                 {
-                    Name = directory.Name,
-                    Path = directory.FullName,
-                    Items = GetItems(directory.FullName)
-                };
+                    var item = new DirectoryItem
+                    {
+                        Name = directory.Name,
+                        Path = directory.FullName,
+                        Items = GetItems(directory.FullName)
+                    };
 
-                items.Add(item);
-            }
+                    items.Add(item);
+                }
 
-            foreach (var file in dirInfo.GetFiles())
-            {
-                var item = new FileItem
+                foreach (var file in dirInfo.GetFiles())
                 {
-                    Name = file.Name,
-                    Path = file.FullName
-                };
+                    var item = new FileItem
+                    {
+                        Name = file.Name,
+                        Path = file.FullName
+                    };
 
-                items.Add(item);
+                    items.Add(item);
+                }
+                */
             }
-            */
             return items;
+        }
+
+        class BuildTreeFromRepo
+        {
+            public List<Item> RecurseRepoItems(List<string> repo_file_list, string working_directory)
+            {
+                Dictionary<string, List<string>> item_files_directories = new Dictionary<string, List<string>>();
+                var items = new List<Item>();
+
+                foreach (var repo_item in repo_file_list)
+                {
+                    // if file in a directory
+                    if (repo_item.IndexOf("\\") > -1)
+                    {
+                        // split the next level directory from the the strinng
+                        string sub_directory_name = repo_item.Substring(0, repo_item.IndexOf("\\"));
+                        string remaining_file_path = repo_item.Substring(repo_item.IndexOf("\\"));
+
+                        // if this directory name has already been added to the item list, find and add remaining string to directory
+                        if (item_files_directories.ContainsKey(sub_directory_name))
+                        {
+                            item_files_directories[sub_directory_name].Add(remaining_file_path);
+                        }
+                        else
+                        {
+                            List<string> sub_directory_file_list = new List<string>();
+                            sub_directory_file_list.Add(remaining_file_path);
+                            item_files_directories.Add(sub_directory_name, sub_directory_file_list);
+                        }
+                        // else create a new directory item and add remaining string to item list
+
+                        items.Add(item);
+                    }
+                    else  // just a file
+                    {
+                        var item = new FileItem
+                        {
+                            Name = repo_item.FilePath,
+                            Path = Path.Combine(working_directory, repo_item.FilePath)
+                        };
+
+                        items.Add(item);
+                    }
+                }
+                return items;
+
+            }
         }
     }
 }
