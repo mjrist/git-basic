ae9a7be6eb08aeb23e9463a08e76737cfbabc93f
gdb13 <gdb.thirteen@gmail.com>
2018-02-28 22:01:42 -0500

    ItemProvider complete. Needs tested.

--- Playground/Lib/ItemProvider.cs
+++ Playground/Lib/ItemProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Playground.Lib.FileSystem;
 using LibGit2Sharp;
+using System;
 
 namespace Playground.Lib
 {
@@ -10,7 +11,7 @@ namespace Playground.Lib
         public List<Item> GetItems(string path)
         {
             var items = new List<Item>();
-;           List<string> repo_items = new List<string>;
+;           List<string> repo_items = new List<string>();
             
             if (Repository.IsValid(path))
             {
@@ -27,31 +28,6 @@ namespace Playground.Lib
                 }
                 items = file_tree.RecurseRepoItems(repo_items, path);
 
-
-                /*
-                foreach (var directory in dirInfo.GetDirectories())
-                {
-                    var item = new DirectoryItem
-                    {
-                        Name = directory.Name,
-                        Path = directory.FullName,
-                        Items = GetItems(directory.FullName)
-                    };
-
-                    items.Add(item);
-                }
-
-                foreach (var file in dirInfo.GetFiles())
-                {
-                    var item = new FileItem
-                    {
-                        Name = file.Name,
-                        Path = file.FullName
-                    };
-
-                    items.Add(item);
-                }
-                */
             }
             return items;
         }
@@ -60,44 +36,57 @@ namespace Playground.Lib
         {
             public List<Item> RecurseRepoItems(List<string> repo_file_list, string working_directory)
             {
-                Dictionary<string, List<string>> item_files_directories = new Dictionary<string, List<string>>();
+                Dictionary<string, List<string>> folders_with_remaining_strings = new Dictionary<string, List<string>>();
                 var items = new List<Item>();
 
                 foreach (var repo_item in repo_file_list)
                 {
                     // if file in a directory
-                    if (repo_item.IndexOf("\\") > -1)
+                    if (repo_item.IndexOf("\\") > 0)
                     {
-                        // split the next level directory from the the strinng
+                        // split the next level directory from the the string
                         string sub_directory_name = repo_item.Substring(0, repo_item.IndexOf("\\"));
-                        string remaining_file_path = repo_item.Substring(repo_item.IndexOf("\\"));
+                        string remaining_file_path = repo_item.Substring(repo_item.IndexOf("\\") + 1);
 
-                        // if this directory name has already been added to the item list, find and add remaining string to directory
-                        if (item_files_directories.ContainsKey(sub_directory_name))
+                        // if this directory name has already been added to the item list, find and add remaining string to directory list
+                        if (folders_with_remaining_strings.ContainsKey(sub_directory_name))
                         {
-                            item_files_directories[sub_directory_name].Add(remaining_file_path);
+                            folders_with_remaining_strings[sub_directory_name].Add(remaining_file_path);
                         }
-                        else
+                        else  // else create a new directory item and add remaining string to item list
                         {
                             List<string> sub_directory_file_list = new List<string>();
                             sub_directory_file_list.Add(remaining_file_path);
-                            item_files_directories.Add(sub_directory_name, sub_directory_file_list);
+                            folders_with_remaining_strings.Add(sub_directory_name, sub_directory_file_list);
                         }
-                        // else create a new directory item and add remaining string to item list
 
-                        items.Add(item);
                     }
                     else  // just a file
                     {
                         var item = new FileItem
                         {
-                            Name = repo_item.FilePath,
-                            Path = Path.Combine(working_directory, repo_item.FilePath)
+                            Name = repo_item.ToString(),
+                            Path = Path.Combine(working_directory, repo_item.ToString())
                         };
 
                         items.Add(item);
                     }
                 }
+
+                // recursively build up remaining directory structures
+                foreach (string key in folders_with_remaining_strings.Keys)
+                {
+                    Console.WriteLine(key);
+                    var item = new DirectoryItem
+                    {
+                        Name = key,
+                        Path = Path.Combine(working_directory, key),
+                        Items = RecurseRepoItems(folders_with_remaining_strings[key], Path.Combine(working_directory, key))
+                    };
+
+                    items.Add(item);
+                }
+
                 return items;
 
             }
