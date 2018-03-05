f4fe8071f83820d3cd0bfe4f3e0ad670a7f169e1
Matthew Ristine <matthewristine@gmail.com>
2018-03-04 22:51:48 -0800

    Adds checkout from repo status bar

---
+++ GitBasic/Controls/RepositoryStatusBar.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GitBasic.Controls
+{
+    /// <summary>
+    /// Interaction logic for StatusControl.xaml
+    /// </summary>
+    public partial class RepositoryStatusBar : UserControl
+    {
+        public RepositoryStatusBar()
+        {
+            InitializeComponent();
+        }
+
+        private void RepoButton_Click(object sender, RoutedEventArgs e)
+        {
+            RepoButtonAction();
+        }
+
+        private void BranchButton_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            InputHelper.SendRightClick(sender);
+        }
+
+        private void BypassRightClick(object sender, MouseButtonEventArgs e) => e.Handled = true;
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string brancName = ((MenuItem)sender).Header.ToString();
+            CheckoutAction(brancName);
+        }
+
+        #region Dependency Properties
+
+        public string RepositoryName
+        {
+            get { return (string)GetValue(RepositoryNameProperty); }
+            set { SetValue(RepositoryNameProperty, value); }
+        }
+        public static readonly DependencyProperty RepositoryNameProperty =
+            DependencyProperty.Register("RepositoryName", typeof(string), typeof(RepositoryStatusBar), new PropertyMetadata(string.Empty));
+
+        public Action RepoButtonAction
+        {
+            get { return (Action)GetValue(RepoButtonActionProperty); }
+            set { SetValue(RepoButtonActionProperty, value); }
+        }
+        public static readonly DependencyProperty RepoButtonActionProperty =
+            DependencyProperty.Register("RepoButtonAction", typeof(Action), typeof(RepositoryStatusBar), new PropertyMetadata(new Action(() => { })));
+
+        public string BranchName
+        {
+            get { return (string)GetValue(BranchNameProperty); }
+            set { SetValue(BranchNameProperty, value); }
+        }
+        public static readonly DependencyProperty BranchNameProperty =
+            DependencyProperty.Register("BranchName", typeof(string), typeof(RepositoryStatusBar), new PropertyMetadata(string.Empty));
+
+        public ObservableCollection<string> BranchNames
+        {
+            get { return (ObservableCollection<string>)GetValue(BranchNamesProperty); }
+            set { SetValue(BranchNamesProperty, value); }
+        }
+        public static readonly DependencyProperty BranchNamesProperty =
+            DependencyProperty.Register("BranchNames", typeof(ObservableCollection<string>), typeof(RepositoryStatusBar), new PropertyMetadata(new ObservableCollection<string>()));
+
+        public Action<string> CheckoutAction
+        {
+            get { return (Action<string>)GetValue(CheckoutActionProperty); }
+            set { SetValue(CheckoutActionProperty, value); }
+        }
+        public static readonly DependencyProperty CheckoutActionProperty =
+            DependencyProperty.Register("CheckoutAction", typeof(Action<string>), typeof(RepositoryStatusBar), new PropertyMetadata(new Action<string>((branchName) => { })));
+
+        #endregion
+    }
+}
---
+++ GitBasic/ViewModels/RepositoryStatusBarVM.cs
@@ -0,0 +1,49 @@
+﻿using Reactive;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GitBasic
+{
+    public class RepositoryStatusBarVM
+    {
+        public Action SelectDirectoryCommand { get; set; }
+        public Action<string> Checkout { get; set; }
+        public ObservableCollection<string> BranchNames { get; set; } = new ObservableCollection<string>();
+
+        public RepositoryStatusBarVM(MainVM mainVM)
+        {
+            _mainVM = mainVM;
+            SelectDirectoryCommand = new Action(SelectDirectory);
+            Checkout = new Action<string>((branchName) => _mainVM.ConsoleControlVM.ExecuteCommand($"{GIT_CHECKOUT} {branchName}"));
+            ReactiveAction branchNamesUpdater = new ReactiveAction(() => App.Current.Dispatcher.Invoke(UpdateBranchNames), _mainVM.Repo, _mainVM.RepoNotifier);
+        }
+
+        private void SelectDirectory()
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.ShowNewFolderButton = false;
+            folderBrowser.SelectedPath = _mainVM.WorkingDirectory.Value;
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                _mainVM.ConsoleControlVM.ExecuteCommand($"cd {folderBrowser.SelectedPath}");
+            }
+        }
+
+        private void UpdateBranchNames()
+        {
+            if (_mainVM.Repo.Value != null)
+            {
+                BranchNames.Clear();
+                var newBranchNames = _mainVM.Repo.Value.Branches.Where(b => !b.IsRemote).Select(b => b.FriendlyName);
+                newBranchNames.ForEach(BranchNames.Add);
+            }
+        }
+
+        private const string GIT_CHECKOUT = "git checkout";
+
+        private MainVM _mainVM;
+    }
+}
