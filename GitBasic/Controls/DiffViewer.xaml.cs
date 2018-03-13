8fe9e98ec37e745be5e8616fed9ffdd31921c0ac
Matthew Ristine <matthewristine@gmail.com>
2018-03-13 00:20:59 -0700

    Finishes diff viewer spacing and coloring.

--- GitBasic/Controls/DiffViewer.xaml.cs
+++ GitBasic/Controls/DiffViewer.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.IO;
+﻿using LibGit2Sharp;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using LibGit2Sharp;
 
 namespace GitBasic.Controls
 {
@@ -25,12 +14,12 @@ namespace GitBasic.Controls
         public DiffViewer()
         {
             InitializeComponent();
+            _oldDiff = new DiffFormatter(OldDiff);
+            _newDiff = new DiffFormatter(NewDiff);
         }
 
         private void Diff(string fileName)
         {
-            // I had to add this because OnFileNameChanged sometimes call this with null or empty fileName.
-            // Feel free to rewrite this function and clean this up.
             if (string.IsNullOrWhiteSpace(fileName))
             {
                 return;
@@ -38,14 +27,13 @@ namespace GitBasic.Controls
 
             string repoPath = Repository.Discover(fileName);
 
-            if (string.IsNullOrEmpty(repoPath)) 
+            if (string.IsNullOrEmpty(repoPath))
             {
                 return;
             }
 
-            using(var repo = new Repository(repoPath)) 
+            using (var repo = new Repository(repoPath))
             {
-                //GitSharp's new file oid doesn't work so this is a workaround.
 
                 var change = repo.Diff.Compare<TreeChanges>(new string[] { fileName }, true).FirstOrDefault();
 
@@ -55,7 +43,7 @@ namespace GitBasic.Controls
                     //Blob newBlob = repo.Lookup<Blob>(change.Oid);
                     string localFile = oldBlob.GetContentText();
                     //string newText = newBlob.GetContentText();
-                    string headFile = File.ReadAllText(fileName);
+                    string headFile = System.IO.File.ReadAllText(fileName);
 
                     // Have to normalize the line endings because LibGit2Sharp is using '\n' but Windows in '\r\n'.
                     localFile = Regex.Replace(localFile, @"\r\n|\n\r|\n|\r", "\r\n");
@@ -69,76 +57,59 @@ namespace GitBasic.Controls
         private void ShowDiff(string oldText, string newText)
         {
             GitSharp.Diff diff = new GitSharp.Diff(oldText, newText);
-            
-            OldDiff.Document.Blocks.Clear();
-            NewDiff.Document.Blocks.Clear();
+
+            _oldDiff.Clear();
+            _newDiff.Clear();
 
             //File name at top of document
-            string fileName = Path.GetFileName(FileName);
+            string fileName = System.IO.Path.GetFileName(FileName);
             oldTitle.Text = $"{fileName} - HEAD";
             newTitle.Text = $"{fileName} - MODIFIED";
 
-
             foreach (var section in diff.Sections)
             {
-                if (section.Status.Equals(GitSharp.Diff.SectionStatus.Unchanged))
+                if (section.Status == GitSharp.Diff.SectionStatus.Unchanged)
                 {
-                    OldDiff.Document.Blocks.Add(new Paragraph(new Run(section.TextA.TrimEnd('\r', '\n'))));
-                    NewDiff.Document.Blocks.Add(new Paragraph(new Run(section.TextB.TrimEnd('\r', '\n'))));
+                    _oldDiff.AddSection(section.TextA);
+                    _newDiff.AddSection(section.TextB);
                 }
-                else if (section.EditWithRespectToA.Equals(GitSharp.Diff.EditType.Replaced))
+                else if (section.EditWithRespectToA == GitSharp.Diff.EditType.Replaced
+                    || section.EditWithRespectToB == GitSharp.Diff.EditType.Replaced)
                 {
                     int sectionASize = section.EndA - section.BeginA;
-                    Run oldRun = new Run(section.TextA.TrimEnd('\r', '\n'));
+                    int sectionBSize = section.EndB - section.BeginB;
 
-                    if (sectionASize != (section.EndB - section.BeginB))
+                    _oldDiff.AddSection(section.TextA, DiffSectionType.Removed);
+                    _newDiff.AddSection(section.TextB, DiffSectionType.Added);
+
+                    if (sectionASize < sectionBSize)
                     {
-
-                        for (int i = 0; i < (section.EndB - section.BeginB - 1); i++)
-                        {
-                            oldRun.Text += Environment.NewLine;
-                        }
+                        int lineCount = sectionBSize - sectionASize;
+                        _oldDiff.AddPadding(lineCount);
                     }
-
-                    Paragraph oldD = new Paragraph(oldRun);
-                    oldD.Background = Brushes.Red;
-                    Paragraph newD = new Paragraph(new Run(section.TextB.TrimEnd('\r', '\n')));
-                    newD.Background = Brushes.Green;
-
-                    OldDiff.Document.Blocks.Add(oldD);
-                    NewDiff.Document.Blocks.Add(newD);
-                }
-                else if (section.EditWithRespectToA.Equals(GitSharp.Diff.EditType.Inserted))
-                {
-                    Run emptySpaces = new Run();
-                    for (int i = 0; i < (section.EndB - section.BeginB - 1); i++)
+                    else if (sectionBSize < sectionASize)
                     {
-                        emptySpaces.Text += Environment.NewLine;
+                        int lineCount = sectionASize - sectionBSize;
+                        _newDiff.AddPadding(lineCount);
                     }
-                    OldDiff.Document.Blocks.Add(new Paragraph(emptySpaces));
-
-                    Paragraph newD = new Paragraph(new Run(section.TextB.TrimEnd('\r', '\n')));
-                    newD.Background = Brushes.Green;
-                    NewDiff.Document.Blocks.Add(newD);
                 }
-                else if (section.EditWithRespectToB.Equals(GitSharp.Diff.EditType.Inserted))
+                else if (section.EditWithRespectToA == GitSharp.Diff.EditType.Inserted)
                 {
-                    Run emptySpaces = new Run();
-                    for (int i = 0; i < (section.EndA - section.BeginA - 1); i++)
-                    {
-                        emptySpaces.Text += Environment.NewLine;
-                    }
-                    OldDiff.Document.Blocks.Add(new Paragraph(new Run(section.TextA.TrimEnd('\r', '\n'))));
-
-                    Paragraph newD = new Paragraph(emptySpaces);
-                    newD.Background = Brushes.Red;
-                    NewDiff.Document.Blocks.Add(newD);
+                    int lineCount = section.EndB - section.BeginB;
+                    _oldDiff.AddPadding(lineCount);
+                    _newDiff.AddSection(section.TextB, DiffSectionType.Added);
                 }
-                else
+                else if (section.EditWithRespectToB == GitSharp.Diff.EditType.Inserted)
                 {
+                    int lineCount = section.EndA - section.BeginA;
+                    _newDiff.AddPadding(lineCount);
+                    _oldDiff.AddSection(section.TextA, DiffSectionType.Removed);
                 }
             }
         }
+
+        private DiffFormatter _oldDiff;
+        private DiffFormatter _newDiff;
 
         #region Dependency Properties
 
@@ -148,16 +119,15 @@ namespace GitBasic.Controls
             set { SetValue(FileNameProperty, value); }
         }
 
-        public static readonly DependencyProperty FileNameProperty = 
+        public static readonly DependencyProperty FileNameProperty =
             DependencyProperty.Register("FileName", typeof(string), typeof(DiffViewer), new PropertyMetadata(string.Empty, OnFileNameChanged));
 
-        private static void OnFileNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) 
-        {            
-            ((DiffViewer) d).Diff((string) e.NewValue);
+        private static void OnFileNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((DiffViewer)d).Diff((string)e.NewValue);
         }
 
         #endregion
-
     }
 }
 
