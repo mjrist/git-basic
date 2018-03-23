5dc9ec0a97bb8b9d75c6b66249ae8a484092efb4
Matthew Ristine <matthewristine@gmail.com>
2018-03-22 22:39:34 -0700

    Removes text wrapping from diff viewer.

--- GitBasic/Controls/DiffViewer.xaml.cs
+++ GitBasic/Controls/DiffViewer.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using LibGit2Sharp;
+using Reactive;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace GitBasic.Controls
 {
@@ -16,6 +19,7 @@ namespace GitBasic.Controls
             InitializeComponent();
             _oldDiff = new DiffFormatter(OldDiff);
             _newDiff = new DiffFormatter(NewDiff);
+            SetupWidthWatchers();
         }
 
         private void Diff(string fileName)
@@ -62,6 +66,9 @@ namespace GitBasic.Controls
             ClearDiffViewer();
             SetDiffTitles();
 
+            _oldDiffTextWidth.Value = GetTextWidth(oldText);
+            _newDiffTextWidth.Value = GetTextWidth(newText);
+
             foreach (var section in diff.Sections)
             {
                 if (section.Status == GitSharp.Diff.SectionStatus.Unchanged)
@@ -104,23 +111,78 @@ namespace GitBasic.Controls
             }
         }
 
-        private void ClearDiffViewer()
-        {
-            _oldDiff.Clear();
-            _newDiff.Clear();
-            oldTitle.Text = string.Empty;
-            newTitle.Text = string.Empty;
-        }
-
         private void SetDiffTitles()
         {
             string fileName = System.IO.Path.GetFileName(FileName);
             oldTitle.Text = $"{fileName} - HEAD";
             newTitle.Text = $"{fileName} - MODIFIED";
         }
+        private void ClearDiffViewer()
+        {
+            _oldDiff.Clear();
+            _newDiff.Clear();
+            _oldDiffTextWidth.Value = 0;
+            _newDiffTextWidth.Value = 0;
+            oldTitle.Text = string.Empty;
+            newTitle.Text = string.Empty;
+        }
 
+        private double GetTextWidth(string text)
+        {
+            FormattedText formattedText = new FormattedText(text,
+                CultureInfo.GetCultureInfo("en-us"),
+                FlowDirection.LeftToRight,
+                new Typeface("Segoe UI"),
+                12,
+                Brushes.WhiteSmoke);
+
+            return formattedText.WidthIncludingTrailingWhitespace + 20;
+        }
+
+        private void LeftScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e) => SetMinWidth(OldDiff, e.NewSize.Width - 4, _oldDiffTextWidth.Value);
+
+        private void RightScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e) => SetMinWidth(NewDiff, e.NewSize.Width - 24, _newDiffTextWidth.Value);
+
+        private void SetMinWidth(RichTextBox textBox, double controlWidth, double textWidth)
+        {
+            textBox.MinWidth = (controlWidth > textWidth) ? controlWidth : textWidth;
+        }
+
+        private void SetupWidthWatchers()
+        {
+            new ReactiveAction(() => SetMinWidth(OldDiff, LeftScrollViewer.ActualWidth - 4, _oldDiffTextWidth.Value), _oldDiffTextWidth);
+            new ReactiveAction(() => SetMinWidth(NewDiff, RightScrollViewer.ActualWidth - 24, _newDiffTextWidth.Value), _newDiffTextWidth);
+        }
+
+        private Prop<double> _oldDiffTextWidth = new Prop<double>(0);
+        private Prop<double> _newDiffTextWidth = new Prop<double>(0);
         private DiffFormatter _oldDiff;
         private DiffFormatter _newDiff;
+
+        private void ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (sender == LeftScrollViewer)
+            {
+                SyncScrollViewer(RightScrollViewer, e);
+            }
+            else // RightScrollViewer
+            {
+                SyncScrollViewer(LeftScrollViewer, e);
+            }
+        }
+
+        private void SyncScrollViewer(ScrollViewer scrollViewer, ScrollChangedEventArgs e)
+        {
+            if (e.VerticalChange != 0)
+            {
+                scrollViewer.ScrollToVerticalOffset(e.VerticalOffset);
+            }
+
+            if (e.HorizontalChange != 0)
+            {
+                scrollViewer.ScrollToHorizontalOffset(e.HorizontalOffset);
+            }
+        }
 
         #region Dependency Properties
 
